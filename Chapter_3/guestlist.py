601d5a734e18e2d159b9ad8f49fc24cf5e01cd73
HaribullaShaik <shaikharibulla984@gmail.com>
2026-02-20 14:16:04 -0600

    Completed list exercies

---
+++ Chapter_3/guestlist.py
@@ -0,0 +1,6 @@
+people = ["Alice", "Bob", "Charlie", "david", "eve"]
+print(f"You are invited to the party, {people[0].title()}!")
+print(f"You are invited to the party, {people[1].title()}!")
+print(f"You are invited to the party, {people[2].title()}!")
+print(f"You are invited to the party, {people[3].title()}!")
+print(f"You are invited to the party, {people[4].title()}!")
