ea3d8ac1cc945a791d92c7ed5f60699fb5d1ff61
HaribullaShaik <shaikharibulla984@gmail.com>
2026-02-20 14:16:38 -0600

    Delete changingguestlist.py

--- changingguestlist.py
+++
@@ -1,6 +0,0 @@
-people = ["Alice", "Bob", "Charlie", "David", "Eve"]
-print(f"You are invited to the party, {people[0].title()}, {people[1].title()}, {people[2].title()}, and {people[3].title()}!")
-print(f"Unfortunately, {people[3].title()} can't make it to the party.")
-people[3] = "Frank"
-print(f"Let's invite {people[3].title()} instead.")
-print(f"You are invited to the party, {people[0].title()}, {people[1].title()}, {people[2].title()}, and {people[3].title()}!")
