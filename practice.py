833bb1f693a0cd1bb4422963d1eb51ce84905bf8
HaribullaShaik <shaikharibulla984@gmail.com>
2026-02-20 16:31:41 -0600

    some exercises are added from chapter_4

---
+++ practice.py
@@ -0,0 +1,5 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()} is a great magician!")
+    print(f"I can't wait to see {magician.title()} perform next!\n")
+print("Thank you, everyone. That was a great magic show!")
