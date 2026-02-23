0648a82dc70ed2056ad736a1153a266811273d80
HaribullaShaik <shaikharibulla984@gmail.com>
2026-02-22 20:54:17 -0600

    eve added

--- practice.py
+++ practice.py
@@ -3,6 +3,7 @@ favourite_languages = {
     "Bob": "JavaScript",
     "Charlie": "C++",
     "David": "Java",
+    "Eve": "Python",
 }
 print("The following languages have been mentioned:")
 for language in sorted(set(favourite_languages.values())):
