c6ae2b6e79eef7c00191fa99bb5a5cc265a62934
HaribullaShaik <shaikharibulla984@gmail.com>
2026-02-26 12:24:26 -0600

    basic function exercises added

---
+++ Chapter_8/message.py
@@ -0,0 +1,4 @@
+def message():
+    """Display a simple message."""
+    print("Hello Python world!, im learning python Functions and Modules")
+message()
--- practice.py
+++ practice.py
@@ -1,14 +1,4 @@
-responses = {}
-polling_active = True
-while polling_active:
-    name = input("\nWhat is your name? ")
-    response = input("Which mountain would you like to climb someday? ")
-
-    responses[name] = response
-
-    repeat = input("Would you like to let another person respond? (yes/no) ")
-    if repeat.lower() == 'no':
-        polling_active = False
-print("\n--- Poll Results ---")
-for name, response in responses.items():
-    print(f"{name} would like to climb {response}.")
+def greet_user(username):
+    """Display a simple greeting."""
+    print(f"Hello, {username.title()}!")
+greet_user(input("What is your name? "))
