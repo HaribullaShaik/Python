9116d1d65199108624728582dad14ac556440042
HaribullaShaik <shaikharibulla984@gmail.com>
2026-02-26 19:27:53 -0600

    Importing modules exercises

---
+++ Chapter_8/imports.py
@@ -0,0 +1,5 @@
+import printing_functions
+from printing_functions import print_models, show_completed_models
+from printing_functions import print_models as pm, show_completed_models as scm
+import printing_functions as pf
+from printing_functions import *
