09beaffa830ea9280fb5f3133605160e6f400fe4
HaribullaShaik <shaikharibulla984@gmail.com>
2026-02-26 18:25:12 -0600

    Using Lists with Function exercieses

---
+++ Chapter_8/messages.py
@@ -0,0 +1,5 @@
+short_text = ["Hi How are you?", "Hello!", "Good morning!"]
+def show_messages(messages):
+    for message in messages:
+        print(message)
+show_messages(short_text)
--- practice.py
+++ practice.py
@@ -1,8 +1,17 @@
-def build_person(first_name, last_name, age=None):
-    """Return a dictionary of information about a person."""
-    person = {'first': first_name, 'last': last_name}
-    if age:
-        person['age'] = age
-    return person
-musician = build_person('jimi', 'hendrix')
-print(musician)
+def print_model(unprinted_designs, completed_models):
+    """Simulate printing each design, until none are left.
+    Move each design to completed_models after printing."""
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+def show_completed_models(completed_models):
+    """Show all the models that were printed."""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+print_model(unprinted_designs[:], completed_models)
+show_completed_models(completed_models)
+print(unprinted_designs)
