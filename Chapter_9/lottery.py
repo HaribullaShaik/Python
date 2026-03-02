04e35cf4a09e05b256f0b79d1fe783ca0176cab1
HaribullaShaik <shaikharibulla984@gmail.com>
2026-03-01 22:00:37 -0600

    python standard library exercises

--- Chapter_9/lottery.py
+++ Chapter_9/lottery.py
@@ -1,19 +1,25 @@
 from random import choice
 
-possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'a', 'b', 'c', 'd', 'e']
+class Lottery:
+    def __init__(self):
+        self.possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'a', 'b', 'c', 'd', 'e']
 
-winning_ticket = []
-print("Let's see what the winning ticket is...")
+    def draw_ticket(self):
+        winning_ticket = []
+        print("Let's see what the winning ticket is...")
 
-# We don't want to repeat winning numbers or letters, so we'll use a
-#   while loop.
-while len(winning_ticket) < 4:
-    pulled_item = choice(possibilities)
+        # We don't want to repeat winning numbers or letters, so we'll use a
+        #   while loop.
+        while len(winning_ticket) < 4:
+            pulled_item = choice(self.possibilities)
 
-    # Only add the pulled item to the winning ticket if it hasn't
-    #   already been pulled.
-    if pulled_item not in winning_ticket:
-        print(f"  We pulled a {pulled_item}!")
-        winning_ticket.append(pulled_item)
+            # Only add the pulled item to the winning ticket if it hasn't
+            #   already been pulled.
+            if pulled_item not in winning_ticket:
+                print(f"  We pulled a {pulled_item}!")
+                winning_ticket.append(pulled_item)
 
-print(f"\nThe final winning ticket is: {winning_ticket}")
+        print(f"\nThe final winning ticket is: {winning_ticket}")
+
+lottery = Lottery()
+lottery.draw_ticket()
--- Chapter_9/lottery_analysis.py
+++ Chapter_9/lottery_analysis.py
@@ -1,27 +1,33 @@
 from random import choice
 
-possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'a', 'b', 'c', 'd', 'e']
+class Lottery:
+    def __init__(self,my_ticket):
+        self.possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'a', 'b', 'c', 'd', 'e']
+        self.my_ticket = my_ticket
+    def draw_ticket(self):
+        print("Let's see what the winning ticket is...")
 
-my_ticket = [1, 3, 'a', 'd']
-print("Let's see what the winning ticket is...")
+        counter = 0
+        max_iterations = 10000
+        while True:
+            counter += 1
+            winning_ticket = []
+            while len(winning_ticket) < 4:
+                pulled_item = choice(self.possibilities)
+                if pulled_item not in winning_ticket:
+                    winning_ticket.append(pulled_item)
 
-counter = 0
-max_iterations = 10000
-while True:
-    counter += 1
-    winning_ticket = []
-    while len(winning_ticket) < 4:
-        pulled_item = choice(possibilities)
-        if pulled_item not in winning_ticket:
-            winning_ticket.append(pulled_item)
+            print(f"\nThe winning ticket is: {winning_ticket}")
+            if winning_ticket == self.my_ticket:
+                print("Congratulations! You win the lottery!")
+                print(f"The while loop was executed {counter} times.")
+                break
+            else:
+                print("Sorry, you didn't win. Try again!")
+            if counter >= max_iterations:
+                print(f"Sorry, you didn't win after {counter} iterations. Better luck next time!")
+                break
 
-    print(f"\nThe winning ticket is: {winning_ticket}")
-    if winning_ticket == my_ticket:
-        print("Congratulations! You win the lottery!")
-        print(f"The while loop was executed {counter} times.")
-        break
-    else:
-        print("Sorry, you didn't win. Try again!")
-    if counter >= max_iterations:
-        print(f"Sorry, you didn't win after {counter} iterations. Better luck next time!")
-        break  
+my_ticket = [1, 2, 3, 4]
+lottery = Lottery(my_ticket)
+lottery.draw_ticket()
